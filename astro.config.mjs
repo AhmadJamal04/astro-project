a00320ec0dc7568a6997a04ba1bc34cc898eed9e
Ahmad Jamal <ENGR.JAMAL04@GMAIL.COM>
2024-06-26 17:33:03 +0500

    integration of preact and rss feed configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://master--admirable-sunflower-8b0b7e.netlify.app/",
+  integrations: [preact()]
+});
